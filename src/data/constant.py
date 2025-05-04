941d99c3792de89c8dbdc372544cbca8c7969e0d
stanley.mbote <stanley.mbote@thejitu.com>
2025-05-05 00:29:27 +0300

    Adding the supabase local client

--- src/data/constant.py
+++ src/data/constant.py
@@ -3,4 +3,8 @@ BASE_URL = "https://api.openf1.org/v1"
 DEFAULT_TIMEOUT = 30  # seconds
 MAX_RETRIES = 3
 
+
+supa_base_url = "https://kqjzvjqxqjzvjqxqjzv.supabase.co"
+
+supa_base_password = "EQ_cbpkH%.@$s3w"
 # supabase connection_passord = "EQ_cbpkH%.@$s3w"
